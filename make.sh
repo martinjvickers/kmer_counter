d9f9c79d17fbee02c772ee1c358fceb9ba38ff2d
Martin Vickers <rudigarude@googlemail.com>
2017-02-15 15:55:08 +0000

    Removed make.sh

--- make.sh
+++
@@ -1,6 +0,0 @@
-###A bit hacky but this allows the make step to fail and then redoes the linking with -ltbb at the end.
-
-
-#!/bin/sh
-make -j4
-/usr/bin/c++ -std=c++14 -O3 -DNDEBUG CMakeFiles/kmer_counter.dir/kmer_counter.cpp.o  -o kmer_counter -rdynamic -lrt -lz -Wl,--whole-archive -ltbb -lpthread -Wl,-no-whole-archive
